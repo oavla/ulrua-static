5b696faee255e74d2bfe4f5ddbff9613ee30000b
ral <123607334+TewaaCao@users.noreply.github.com>
2024-11-27 10:48:31 -0600

    ui and iframe fix

---
+++ js/load.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('searchInput');
+    const gitHub = document.getElementById('gitHub');
+    const engineDropdown = document.getElementById('searchEngine');
+    setTimeout(() => {
+      searchInput.classList.add('expanded');
+    }, 500);
+    setTimeout(() => {
+      engineDropdown.classList.add('expanded');
+    }, 500);
+    setTimeout(() => {
+      gitHub.classList.add('spin');
+    }, 500);
+    const sloganElement = document.getElementById('slogan');
+    const sloganText = '"Browse Safely, Explore Freely."';
+    let i = 0;
+  
+    function typeWriter() {
+      if (i < sloganText.length) {
+        sloganElement.innerHTML += sloganText.charAt(i);
+        i++;
+        setTimeout(typeWriter, 60); 
+      } else {
+        setTimeout(() => {
+          sloganElement.classList.add('cursor-fade-out');
+        }, 1000); 
+      }
+    }
+    typeWriter(); 
+  });
+  
