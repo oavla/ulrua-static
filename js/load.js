ffe117430e424164644b3b8d6b2ae5e91ced95ef
ral <123607334+TewaaCao@users.noreply.github.com>
2024-11-27 13:00:06 -0600

    time

--- js/load.js
+++ js/load.js
@@ -1,31 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const searchInput = document.getElementById('searchInput');
-    const gitHub = document.getElementById('gitHub');
-    const engineDropdown = document.getElementById('searchEngine');
-    setTimeout(() => {
-      searchInput.classList.add('expanded');
-    }, 500);
-    setTimeout(() => {
-      engineDropdown.classList.add('expanded');
-    }, 500);
-    setTimeout(() => {
-      gitHub.classList.add('spin');
-    }, 500);
-    const sloganElement = document.getElementById('slogan');
-    const sloganText = '"Browse Safely, Explore Freely."';
-    let i = 0;
-  
-    function typeWriter() {
-      if (i < sloganText.length) {
-        sloganElement.innerHTML += sloganText.charAt(i);
-        i++;
-        setTimeout(typeWriter, 60); 
-      } else {
-        setTimeout(() => {
-          sloganElement.classList.add('cursor-fade-out');
-        }, 1000); 
-      }
+  const searchInput = document.getElementById('searchInput');
+  const gitHub = document.getElementById('gitHub');
+  const engineDropdown = document.getElementById('searchEngine');
+  setTimeout(() => {
+    searchInput.classList.add('expanded');
+  }, 200);
+  setTimeout(() => {
+    engineDropdown.classList.add('expanded');
+  }, 200);
+  setTimeout(() => {
+    gitHub.classList.add('spin');
+  }, 200);
+  const sloganElement = document.getElementById('slogan');
+  const sloganText = '"Browse Safely, Explore Freely."';
+  let i = 0;
+
+  function typeWriter() {
+    if (i < sloganText.length) {
+      sloganElement.innerHTML += sloganText.charAt(i);
+      i++;
+      setTimeout(typeWriter, 60); 
+    } else {
+      setTimeout(() => {
+        sloganElement.classList.add('cursor-fade-out');
+      }, 1000); 
     }
-    typeWriter(); 
-  });
-  
+  }
+  typeWriter(); 
+});
