fb1b5638207db48c77169920262bb692d6e6aa5b
𓍼 <123607334+oavla@users.noreply.github.com>
2024-11-27 21:31:30 -0600

    Is 9 pm bro

--- js/load.js
+++ js/load.js
@@ -2,29 +2,42 @@ document.addEventListener('DOMContentLoaded', function() {
   const searchInput = document.getElementById('searchInput');
   const gitHub = document.getElementById('gitHub');
   const engineDropdown = document.getElementById('searchEngine');
-  setTimeout(() => {
-    searchInput.classList.add('expanded');
-  }, 200);
-  setTimeout(() => {
-    engineDropdown.classList.add('expanded');
-  }, 200);
-  setTimeout(() => {
-    gitHub.classList.add('spin');
-  }, 200);
+  
+  setTimeout(() => searchInput.classList.add('expanded'), 200);
+  setTimeout(() => engineDropdown.classList.add('expanded'), 200);
+  setTimeout(() => gitHub.classList.add('spin'), 200);
+  
   const sloganElement = document.getElementById('slogan');
-  const sloganText = '"Browse Safely, Explore Freely."';
+  const quotes = [
+    '"The only way to do great work is to love what you do." - Steve Jobs',
+    '"Success is not the key to happiness. Happiness is the key to success." - Albert Schweitzer',
+    '"Believe you can and you’re halfway there." - Theodore Roosevelt',
+    '"Your time is limited, don’t waste it living someone else’s life." - Steve Jobs',
+    '"Dream big and dare to fail." - Norman Vaughan'
+  ];
+  let currentQuoteIndex = 0;
   let i = 0;
 
   function typeWriter() {
-    if (i < sloganText.length) {
-      sloganElement.innerHTML += sloganText.charAt(i);
+    if (i < quotes[currentQuoteIndex].length) {
+      sloganElement.innerHTML += quotes[currentQuoteIndex].charAt(i);
       i++;
-      setTimeout(typeWriter, 60); 
+      setTimeout(typeWriter, 60);
     } else {
       setTimeout(() => {
         sloganElement.classList.add('cursor-fade-out');
-      }, 1000); 
+        setTimeout(nextQuote, 1000); 
+      }, 1000);
     }
   }
+
+  function nextQuote() {
+    sloganElement.classList.remove('cursor-fade-out');
+    sloganElement.innerHTML = '';
+    i = 0;
+    currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length; 
+    typeWriter();
+  }
+
   typeWriter(); 
 });
